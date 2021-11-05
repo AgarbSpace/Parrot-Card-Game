12e128b1dec39d332a6c76e8a1ae8871aa0e8b83
Luis Henrique <luishenrinquebraga@gmail.com>
2021-11-05 13:34:06 -0300

    Criando a pasta assets, implementando o modelo das cartas pelo css e colocando número de cartas selecionadas pelo usuário através do imput na tela

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let cartas = [];
+
 function iniciarJogo(){
    
     let numeroCartas = parseInt(prompt("Qual o número de cartas desejado?"));
@@ -8,4 +10,29 @@ function iniciarJogo(){
             numeroCartas = parseInt(prompt("Número inválido!! Escolha um número par entre 4 e 14:"));
         }while((numeroCartas%2) === 1 || numeroCartas < 4 || numeroCartas > 14);
     }
+
+    if (numeroCartas === 4 || numeroCartas === 6 || numeroCartas === 8 || numeroCartas === 10 || numeroCartas === 12 || numeroCartas === 14) {
+        const cartas = document.querySelector(".jogoIniciado");
+        
+        for(let i = 0; i < numeroCartas; i++){
+            cartas.innerHTML += ` 
+            <div class="card">
+                <div class="front-face face">
+                    <img src="assets/front.png" alt="">
+                </div>
+                <div class="back-face face">
+                    Verso
+                </div>
+            </div>
+        `
+        }
+    }
+}
+
+function none(){
+    const tiraPrimeiraTela = document.querySelector(".conteudo");
+    tiraPrimeiraTela.classList.add("none");
+    const telaJogoAparece = document.querySelector(".jogoInicia");
+    telaJogoAparece.classList.remove("none");
+    telaJogoAparece.classList.add("jogo");
 }
