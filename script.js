bbf4da69fbc803215fbcb36d28236538733be308
Luis Henrique <luishenrinquebraga@gmail.com>
2021-11-08 13:17:19 -0300

    Adicionando a função de virar carta através do click e implementando as demais funções necessárias para o jogo

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let cartasJogo = ['<img src="assets/bobrossparrot.gif" alt="">','<img src="assets/explodyparrot.gif" alt="">','<img src="assets/fiestaparrot.gif" alt="">','<img src="assets/metalparrot.gif" alt="">','<img src="assets/revertitparrot.gif" alt="">','<img src="assets/tripletsparrot.gif" alt="">','<img src="assets/unicornparrot.gif" alt="">'];
+let cartasJogoRepetidas = [];
+let k = 0;
+let primeiraCartaSelecionada;
+let segundaCartaSelecionada;
+let verifica = 0;
+let jogadas = 0;
+let fim = 0;
+let numeroCartas = 0;
 
 function iniciarJogo(){
    
-    let numeroCartas = parseInt(prompt("Qual o número de cartas desejado?"));
-   
+    numeroCartas = parseInt(prompt("Qual o número de cartas desejado?"));
     
     if((numeroCartas%2) === 1 || numeroCartas < 4 || numeroCartas > 14){
         do{
@@ -11,30 +18,26 @@ function iniciarJogo(){
         }while((numeroCartas%2) === 1 || numeroCartas < 4 || numeroCartas > 14);
     }
 
-    if (numeroCartas === 4 || numeroCartas === 6 || numeroCartas === 8 || numeroCartas === 10 || numeroCartas === 12 || numeroCartas === 14) {
+    if (numeroCartas >= 4 && numeroCartas <= 14) {
         const cartas = document.querySelector(".jogoIniciado");
         
-        for(let i = 0; i < (numeroCartas/2); i++){
-            cartas.innerHTML += ` 
-            <div class="card">
-                <div class="front-face face">
-                    <img src="assets/front.png" alt="">
-                </div>
-                <div class="back-face face">
-                    ${cartasJogo[i]}
-                </div>
-            </div>
-        `
+        for(let i = 0; i < numeroCartas; i++){
+            if(i%2===0 && i !== 0){
+                k++;
+            }
+            cartasJogoRepetidas[i] = cartasJogo[k];
         }
 
-        for(let j = 0; j < numeroCartas/2; j++){
+        cartasJogoRepetidas.sort(embaralhar);
+
+        for(let j = 0; j < numeroCartas; j++){
             cartas.innerHTML += ` 
-            <div class="card">
+            <div class="card" onclick = "virar(this)">
                 <div class="front-face face">
                     <img src="assets/front.png" alt="">
                 </div>
                 <div class="back-face face">
-                    ${cartasJogo[j]}
+                    ${cartasJogoRepetidas[j]}
                 </div>
             </div>
         `
@@ -42,10 +45,70 @@ function iniciarJogo(){
     }
 }
 
+
 function none(){
     const tiraPrimeiraTela = document.querySelector(".conteudo");
     tiraPrimeiraTela.classList.add("none");
     const telaJogoAparece = document.querySelector(".jogoInicia");
     telaJogoAparece.classList.remove("none");
     telaJogoAparece.classList.add("jogo");
+}
+
+function virar(carta){
+    carta.classList.add("virar");
+
+    if(verifica === 0){
+        verifica = 1;
+        primeiraCartaSelecionada = carta;
+        return;
+    }
+
+    segundaCartaSelecionada = carta;
+    verifica = 0;
+
+    pares();
+
+    setTimeout(win, 300);
+}
+
+
+function embaralhar(){
+    return Math.random() - 0.5;
+}
+
+function pares(){
+    jogadas++;
+
+    if(primeiraCartaSelecionada.lastElementChild.innerHTML === segundaCartaSelecionada.lastElementChild.innerHTML){
+        primeiraCartaSelecionada.classList.remove("virar");
+        primeiraCartaSelecionada.classList.add("virada");
+        segundaCartaSelecionada.classList.remove("virar");
+        segundaCartaSelecionada.classList.add("virada");
+        fim++;
+        console.log(numeroCartas);
+        console.log(fim);
+   
+        let teste = "ta entrando"
+        console.log(teste);
+        return;
+    }else{
+        desvira();
+        let teste2 = "ta entrando no desvira"
+        console.log(teste2);
+    }
+
+}
+
+
+function desvira(){
+    setTimeout(() => {
+        primeiraCartaSelecionada.classList.remove('virar');
+        segundaCartaSelecionada.classList.remove('virar');
+      }, 1000);
+}
+
+function win(){
+    if((fim) === (numeroCartas/2)){
+        alert("Parabéns!! Você finalizou o jogo em "+ jogadas + " jogadas ^~^");
+    }
 }
